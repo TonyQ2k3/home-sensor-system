cea46802c77eba9af829e56dcd8079e3d075c1fe
TonyQ2k3 <ntbquoc2015@gmail.com>
2023-12-06 09:24:19 +0700

    Remove config

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,14 +3,7 @@ import { getAuth } from 'firebase/auth'
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCP0MaWsAZhpryeiiVGgGiNK7lYPgqUf8Q",
-  authDomain: "adruino-7b5a8.firebaseapp.com",
-  databaseURL: "https://adruino-7b5a8-default-rtdb.firebaseio.com",
-  projectId: "adruino-7b5a8",
-  storageBucket: "adruino-7b5a8.appspot.com",
-  messagingSenderId: "183983165199",
-  appId: "1:183983165199:web:5e64cdd69931c0c162787a",
-  measurementId: "G-CRZLSKV6VV"
+  // Insert config here
 };
 
 const app = initializeApp(firebaseConfig);
